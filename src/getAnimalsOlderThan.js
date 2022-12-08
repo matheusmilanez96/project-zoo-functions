2e498d67ada52c496abdc3434e0983cf25f78651
Matheus Milanez <mattmila96@gmail.com>
2022-12-08 18:19:48 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animalArray = data.species.filter((especie) => especie.name === animal)[0].residents;
+  return animalArray.every((animals) => animals.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
