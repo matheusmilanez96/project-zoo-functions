6f16da1ef37537ccbccdde364a96b9bec6097ffc
Matheus Milanez <mattmila96@gmail.com>
2022-12-12 14:52:16 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter((pessoa) => pessoa.age < 18).length;
+  const adult = entrants.filter((pessoa) => pessoa.age >= 18 && pessoa.age < 50).length;
+  const senior = entrants.filter((pessoa) => pessoa.age >= 50).length;
+  const obj = {
+    child,
+    adult,
+    senior,
+  };
+  return obj;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === 0 || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const ages = countEntrants(entrants);
+  const total = ages.child * 20.99 + ages.adult * 49.99 + ages.senior * 24.99;
+  return total;
 };
 
 module.exports = { calculateEntry, countEntrants };
