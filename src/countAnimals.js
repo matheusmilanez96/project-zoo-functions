054ad1a3d8d24470fc631fd6b926bc9353e113dd
Matheus Milanez <mattmila96@gmail.com>
2022-12-11 23:42:35 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (...animal) => {
+  if (animal.length === 0) {
+    const obj = {};
+    const animals = data.species.map((especie) => {
+      const key = especie.name;
+      obj[key] = especie.residents.length;
+      return obj;
+    });
+    return animals[0];
+  }
+  const animals = data.species.filter((especie) => animal[0].species === especie.name);
+  if (Object.keys(animal[0]).length === 1) {
+    return animals[0].residents.length;
+  }
+  const genderAnimals = animals[0].residents.filter((residente) => residente.sex === animal[0].sex);
+  return genderAnimals.length;
 };
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
