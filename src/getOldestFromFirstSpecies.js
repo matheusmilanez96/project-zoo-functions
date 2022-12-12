ccdee3d3e4d2e2b9dc6867b3ca9226ff37262da6
Matheus Milanez <mattmila96@gmail.com>
2022-12-12 16:51:33 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const currentEmployee = data.employees.find((employee) => employee.id === id);
+  const speciesID = currentEmployee.responsibleFor[0];
+  const currentSpecies = data.species.find((especie) => especie.id === speciesID);
+  const oldestAnimal = currentSpecies.residents
+    .reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  return Object.values(oldestAnimal);
 };
 
 module.exports = getOldestFromFirstSpecies;
